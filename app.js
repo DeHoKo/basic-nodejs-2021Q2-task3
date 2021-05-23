7b1c6b07c303b045772f12ada068b0c92100d17d
DeHoKo <zenkovsk@gmail.com>
2021-05-23 20:06:53 +0300

    fix: erros in app.js

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-var express = require('express');
-var app = express();
-var db = require('./db');
-var user = require('./controllers/usercontroller');
-var game = require('./controllers/gamecontroller')
+require("dotenv").config();
+const express = require("express");
+const validateSession = require("./middleware/validate-session");
+const app = express();
+const db = require("./db");
+const user = require("./controllers/usercontroller");
+const game = require("./controllers/gamecontroller");
 
+const PORT = process.env.PORT || 4000;
 
-db.sync();
-app.use(require('body-parser'));
-app.use('/api/auth', user);
-app.use(require('./middleware/validate-session'))
-app.use('/api/game', game);
-app.listen(function() {
-    console.log("App is listening on 4000");
-})
+db.sync()
+  .then(() => {
+    app.use(express.json());
+    app.use("/api/auth", user);
+    app.use(validateSession);
+    app.use("/api/game", game);
+    app.listen(PORT, function () {
+      console.log(`App is listening on ${PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.error("Can't synchronize ", e);
+  });
